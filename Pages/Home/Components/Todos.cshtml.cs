1559cc6e6b75c3fe7c676794131bd393af044651
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2024-06-07 14:33:42 -0400

    Add delete functionality and header to ToDo list
    One issue is adding client side confirmation. Since Hydro writes out the x:on-click event, there's no way to wrap the generated event with a call to `confirm`.

--- Pages/Home/Components/Todos.cshtml.cs
+++ Pages/Home/Components/Todos.cshtml.cs
@@ -5,16 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HydroTodo.Pages.Home.Components;
 
-public class Todos(Database db) : HydroComponent
+public class Todos(Database db, ILogger<Todos> logger) : HydroComponent
 {
     public override async Task RenderAsync()
     {
         Items = await db.Todos.ToListAsync();
     }
 
-    [Required]
-    public string NewItem { get; set; }
-    
+    [Required] public string NewItem { get; set; }
+
     public List<Todo> Items { get; set; }
 
     public async Task Add()
@@ -27,18 +26,27 @@ public class Todos(Database db) : HydroComponent
         var todo = new Todo { Content = NewItem };
         db.Todos.Add(todo);
         await db.SaveChangesAsync();
-        
+
         NewItem = string.Empty;
     }
 
     public async Task Toggle(int id)
     {
         var todo = await db.Todos.FindAsync(id);
-        if (todo is not null) {
+        if (todo is not null)
+        {
             todo.Done = !todo.Done;
             await db.SaveChangesAsync();
         }
     }
+
+    public async Task Delete(int id)
+    {
+        var todo = await db.Todos.FindAsync(id);
+        if (todo is not null)
+        {
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+        }
+    }
 }
-
-
